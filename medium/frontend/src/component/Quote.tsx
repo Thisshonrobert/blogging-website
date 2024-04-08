248faeaffaeecd0c981d3f856f08a998617d0fed
Thisshonrobert <thisshonrobert0205@gmail.com>
2024-04-08 21:20:47 +0530

    frontend initialized

---
+++ medium/frontend/src/component/Quote.tsx
@@ -0,0 +1,21 @@
+export const Quote = ()=>{
+    return(
+        <div className="bg-gray-200">
+            <div className="flex flex-col w-full h-screen ">
+                <div className="font-medium text-4xl">
+                    "The Customer Service I received was exceptional.The Support Team went above and beyond to address my concerns."
+                </div>
+                <div>
+                    <div className="flex flex-col">
+                        <div>
+
+                        </div>
+                        <div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
